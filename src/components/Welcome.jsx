31c9b5d6609692cac0d9cf2cfe2ae4462db99540
lucasmiguelabreu <abreu.lucasrodrigues@gmail.com>
2024-12-04 12:33:50 -0300

    construção da caixa de perguntas

--- src/components/Welcome.jsx
+++ src/components/Welcome.jsx
@@ -1,16 +1,22 @@
+import { useContext } from 'react';
+import { QuizContext } from '../context/quiz';
+
 import Quiz from '../img/quiz.svg';
 import './Welcome.css';
 
 function Welcome() {
-    
+    const [quizState, dispatch] = useContext(QuizContext);
+
     return (
         <div id="welcome">
             <h1>Seja Bem-vindo!</h1>
             <p>Clique no botão abaixo para começar</p>
-            <button>Iniciar</button>
+            <button onClick={() => dispatch({ type: "CHANGE_STATE" })}>Iniciar</button>
             <img src={Quiz} alt="Inico do Quiz"/>
         </div>
     )
 }
 
 export default Welcome;
+
+// 32:02
